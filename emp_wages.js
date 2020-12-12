bca1eda1d2d4f6e69b996c4c902ac3fa82cc6a91
syed_umair98 <syed.umair.anwar@antronsys.com>
2020-12-12 18:36:23 +0530

    Added:condition of maximum employee upto 160 hour

--- emp_wages.js
+++ emp_wages.js
@@ -4,10 +4,14 @@ const PART_TIME_HOUR=4
 const FULL_TIME_HOUR=8
 const WAGE_PER_HOUR=20
 const NUM_OF_WORKING_DAYS=20
+const MAX_HRS_IN_MONTH=160
+const MAX_OF_WORKING_DAYS=20
 let empHrs=0
-for(let i=0;i<NUM_OF_WORKING_DAYS;i++){
+let total_working_days=0
+while(empHrs<=MAX_HRS_IN_MONTH && total_working_days<NUM_OF_WORKING_DAYS){
 let empCheck=Math.floor(Math.random()*10)%3
 empHrs +=getWorkingHours(empCheck)
+total_working_days++
 }
 function getWorkingHours(empCheck){
  switch (empCheck){
