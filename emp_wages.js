4b7a8c699c05af7be59d7614fa6de4b1d9383e19
syed_umair98 <syed.umair.anwar@antronsys.com>
2020-12-12 14:20:17 +0530

    Added:function to calculate the wages

--- emp_wages.js
+++ emp_wages.js
@@ -1,1 +1,19 @@
-console.log("Welcome To Emp wages program")
+const IS_PART_TIME=1
+const IS_FULL_TIME=2
+const PART_TIME_HOUR=4
+const FULL_TIME_HOUR=8
+const WAGE_PER_HOUR=20
+let empHrs=0
+empCheck=Math.floor(Math.random()*10)%3
+function getWorkingHours(empCheck){
+ switch (empCheck){
+    case IS_PART_TIME:return PART_TIME_HOUR
+        break
+    case IS_FULL_TIME:return FULL_TIME_HOUR
+        break
+    default:return 0        
+ }
+} 
+empHrs=getWorkingHours(empCheck)
+let empWage=empHrs*WAGE_PER_HOUR
+console.log("Emp Wage",empWage)
