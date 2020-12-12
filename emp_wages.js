c56a600262d75a785bdacdad15f3e4de61a27331
syed_umair98 <syed.umair.anwar@antronsys.com>
2020-12-12 22:17:38 +0530

    Added:Daily employee wages in the array

--- emp_wages.js
+++ emp_wages.js
@@ -6,12 +6,18 @@ const WAGE_PER_HOUR=20
 const NUM_OF_WORKING_DAYS=20
 const MAX_HRS_IN_MONTH=160
 const MAX_OF_WORKING_DAYS=20
-let empHrs=0
+let total_empHrs=0
 let total_working_days=0
-while(empHrs<=MAX_HRS_IN_MONTH && total_working_days<NUM_OF_WORKING_DAYS){
+let daily_wage_arr=new Array()
+while(total_empHrs<=MAX_HRS_IN_MONTH && total_working_days<NUM_OF_WORKING_DAYS){
 let empCheck=Math.floor(Math.random()*10)%3
-empHrs +=getWorkingHours(empCheck)
+let empHrs =getWorkingHours(empCheck)
+total_empHrs +=empHrs
 total_working_days++
+daily_wage_arr.push(cal_daily_wages(empHrs))
+}
+function cal_daily_wages(emphrs){
+    return emphrs*WAGE_PER_HOUR
 }
 function getWorkingHours(empCheck){
  switch (empCheck){
@@ -22,5 +28,5 @@ function getWorkingHours(empCheck){
     default:return 0        
  }
 } 
-let empWage=empHrs*WAGE_PER_HOUR
-console.log("Emp Wage",empWage)
+let emp_wage=cal_daily_wages(total_empHrs)
+console.log("Total days is ",total_working_days,"Total hours ",total_empHrs,"total emp wage ",emp_wage)
